2dcc8f7941ef00d94f2c5fcb15b9b3dadee01204
deadlynch <brighterdeath99@gmail.com>
2026-05-13 16:31:06 -0300

    versao 2.0

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
-# Cria o app.js
-@"
 const http = require('http')
 const os = require('os')
 
 const server = http.createServer((req, res) => {
-  res.end('Versao 1.0 - Servidor: ' + os.hostname() + '\n')
+  res.end('Versao 2.0 - Servidor: ' + os.hostname() + '\n')
 })
 
 server.listen(3000, () => console.log('Rodando na porta 3000'))
-"@ | Out-File -FilePath app.js -Encoding utf8
