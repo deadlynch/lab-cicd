3f5c5d0430616934c4a9ef583461c275caddaf14
deadlynch <brighterdeath99@gmail.com>
2026-05-13 16:45:32 -0300

    versao 3.0

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http')
 const os = require('os')
 
 const server = http.createServer((req, res) => {
-  res.end('Versao 2.0 - Servidor: ' + os.hostname() + '\n')
+   res.end('Versao 3.0 - Servidor: ' + os.hostname() + '\n')
 })
 
 server.listen(3000, () => console.log('Rodando na porta 3000'))
